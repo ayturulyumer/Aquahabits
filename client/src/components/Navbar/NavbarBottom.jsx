d533a9bed0ea72b8b08a3980147af9d1e1308921
ajummer <makeudreamempty@gmail.com>
2024-12-04 12:25:54 +0200

    feat: add bot top navbar

---
+++ client/src/components/Navbar/NavbarBottom.jsx
@@ -0,0 +1,19 @@
+import HomeIcon from "../../svg/home-icon.svg"
+import AddIcon from "../../svg/add-icon.svg"
+import HabitsIcon from "../../svg/habits-icon.svg"
+
+export default function NavbarBottom() {
+    return (
+        <div className="btm-nav bg-inherit  ">
+            <button>
+                <img className="w-6 h-6" src={HomeIcon} alt="Home Icon" />
+            </button>
+            <button  >
+                <img className="w-16 h-16 " src={AddIcon} alt="Add Icon" />
+            </button>
+            <button>
+                <img className="w-6 h-6" src={HabitsIcon} alt="Habits Icon" />
+            </button>
+        </div>
+    )
+}
---
+++ client/src/components/Navbar/NavbarTop.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import habitLogo from '../../assets/habitlogo.png';
+import BurgerIcon from '../../svg/burger-menu.svg';
+import CloseIcon from '../../svg/close-icon.svg';
+
+
+export default function NavbarTop() {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const handleToggleMenu = () => {
+        setShowMenu((prevState) => !prevState);
+    };
+
+    return (
+        <nav className="navbar ">
+            <div className="navbar-start mx-8">
+                <Link to="/" className="text-3xl font-light tracking-normal ">Habitect</Link>
+            </div>
+            <div className="navbar-center">
+                <ul className="flex gap-20 mr-4">
+                    <li className="hover-red-underline cursor-pointer">Pricing</li>
+                    <li className="hover-red-underline cursor-pointer">FAQ</li>
+                    <li className="hover-red-underline cursor-pointer">About</li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <ul className="flex mx-20">
+                    <button className="btn btn-md btn-outline  text-white">Login</button>
+                </ul>
+            </div>
+        </nav>
+
+
+
+
+    );
+}
