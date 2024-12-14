3485178ee284ba89f187250c5ed563a90c36660a
ajummer <makeudreamempty@gmail.com>
2024-12-14 21:17:29 +0200

    feat: connect server to mongodb db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
+const DB_URL = process.env.DB_URL;
 
 const routes = require("./routes.js");
 
@@ -6,6 +10,9 @@ const app = express();
 
 app.use(routes);
 
-
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("Successfully connected to DB"))
+  .catch((err) => console.log(err));
 
 app.listen(3030, () => console.log("Server listening"));
