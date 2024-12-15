3f0c82663b82a45f9066eb104e488bb2b7001dfd
ajummer <makeudreamempty@gmail.com>
2024-12-15 12:42:30 +0200

    chore: add custom useForm hook

--- client/src/hooks/useForm.jsx
+++ client/src/hooks/useForm.jsx
@@ -3,15 +3,17 @@ import { useState } from "react";
 export const useForm = (initialValues, onSubmitHandler) => {
     const [values, setValues] = useState(initialValues)
 
+
+    // It also trims the newly set value to prevent empty spaces
     const changeHandler = (e) => {
-        setValues((state) => ({ ...state, [e.target.name]: e.target.value }))
+        setValues((state) => ({ ...state, [e.target.name]: e.target.value.trim() }))
     }
 
     const onSubmit = (e) => {
         e.preventDefault();
         onSubmitHandler(values)
-        setValues(initialValues)
     }
+
 
     return {
         values,
