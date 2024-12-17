9cac9c1c4b34f12fb8a6635fb666d777fe0b1c65
ajummer <makeudreamempty@gmail.com>
2024-12-17 11:34:12 +0200

    feat: create auth api

---
+++ client/src/api/authApi.js
@@ -0,0 +1,10 @@
+const baseUrl = import.meta.env.VITE_APP_BASEURL;
+import * as request from "../lib/requester.js";
+
+export const register = async (email, password) => {
+  const result = await request.post(`${baseUrl}/users/signup`, {
+    email,
+    password,
+  });
+  return result;
+};
