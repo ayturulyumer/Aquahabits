fdaa43f62d647bc12edb8d6d8daddc42becb9b0d
ajummer <makeudreamempty@gmail.com>
2024-12-17 11:35:41 +0200

    fix: add expiration date to cookie

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -8,11 +8,12 @@ router.post("/signup", async (req, res) => {
       email,
       password
     );
-    
+
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true, // prevents client-side from accessing the cookie
-      secure: true, // cookie is sent  only over https
-      sameSite: "strict", // The cookie is only sent for same-site requests, preventing cross-origin misuse.
+      secure: false, // cookie is sent  only over https
+      sameSite: "none", // The cookie is only sent for same-site requests, preventing cross-origin misuse. - CHANGE LATER
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiration
     });
 
     // Send access token to the client
@@ -21,7 +22,6 @@ router.post("/signup", async (req, res) => {
     const statusCode = err.message === "User already exists" ? 400 : 500;
     res.status(statusCode).json({ message: err.message });
   }
-  res.end();
 });
 
 router.get("/", async (req, res) => {
