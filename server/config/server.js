4662849377484e644f95c831465dae09c1503bcd
ajummer <makeudreamempty@gmail.com>
2024-12-17 11:32:14 +0200

    chore: config cors

--- server/config/server.js
+++ server/config/server.js
@@ -2,11 +2,18 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const corsOptions = {
+  origin: "http://localhost:5173", // Allow requests from frontend
+  methods: "GET,POST,PUT,DELETE", // You can specify methods you want to allow
+  allowedHeaders: "Content-Type, Authorization", // You can specify headers you want to allow
+  credentials: true, // Allow cookies if needed
+};
+
 const configureServer = (app) => {
   app.use(express.json()); // Parse JSON
   app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
   app.use(cookieParser()); // Parse cookies
-  app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+  app.use(cors(corsOptions));
 
   console.log("Server configuration loaded successfully !");
 };
