b1b2fc839e247a45d95b6eee7baecd74bfa625f0
ajummer <makeudreamempty@gmail.com>
2024-12-19 17:26:10 +0200

    fix: persist refresh token in cookie

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -14,7 +14,7 @@ router.post("/signup", async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true, // prevents client-side from accessing the cookie
       secure: false, // cookie is sent  only over https
-      sameSite: "none", // The cookie is only sent for same-site requests, preventing cross-origin misuse. - CHANGE LATER
+      sameSite: "lax", // The cookie is only sent for same-site requests, preventing cross-origin misuse. - CHANGE LATER
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiration
     });
 
@@ -45,7 +45,7 @@ router.post("/login", async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: false,
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
