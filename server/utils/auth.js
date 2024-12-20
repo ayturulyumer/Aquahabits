f84d24c6e9e0747c72f3a40e4c0afd731c02c9ed
ajummer <makeudreamempty@gmail.com>
2024-12-20 11:25:23 +0200

    chore: move set refresh token func to util

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,6 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
 
-
 // Generates both refresh/accessToken depending on the payload and expiration date
 const generateToken = (user, expiresIn) => {
   return jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, {
@@ -9,4 +8,13 @@ const generateToken = (user, expiresIn) => {
   });
 };
 
-module.exports = { generateToken };
+const setRefreshToken = (refreshToken) => {
+  return res.cookie("refreshToken", refreshToken, {
+    httpOnly: true, // prevents client-side from accessing the cookie
+    secure: false, // cookie is sent  only over https
+    sameSite: "lax", // The cookie is only sent for same-site requests, preventing cross-origin misuse. - CHANGE LATER
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiration
+  });
+};
+
+module.exports = { generateToken, setRefreshToken };
