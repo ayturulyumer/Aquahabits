0fb69e2f0a85f8f7ce887a2a9011726c6a47baf2
ajummer <makeudreamempty@gmail.com>
2025-01-18 13:09:26 +0200

    feat: add indicator without functionality for quests menu

--- client/src/components/SidebarNav/SidebarNav.jsx
+++ client/src/components/SidebarNav/SidebarNav.jsx
@@ -1,14 +1,12 @@
-
 import { NavLink } from "react-router-dom";
 
-import habitIcon from "../../svg/habit-icon.svg"
-import missionIcon from "../../svg/mission-icon.svg"
-import oceanIcon from "../../svg/ocean-icon.svg"
-
+import habitIcon from "../../svg/habit-icon.svg";
+import missionIcon from "../../svg/mission-icon.svg";
+import oceanIcon from "../../svg/ocean-icon.svg";
 
 const menuItems = [
     { name: "Habits", href: "/dashboard/my-habits", icon: habitIcon, iconAlt: "Habits Icon" },
-    { name: "Quests", href: "/dashboard/my-quests", icon: missionIcon, iconAlt: "Mission Icon" },
+    { name: "Quests", href: "/dashboard/my-quests", icon: missionIcon, iconAlt: "Mission Icon", showIndicator: true },
     { name: "Aquarium", href: "/dashboard/my-aquarium", icon: oceanIcon, iconAlt: "Aquarium Icon" },
 ];
 
@@ -18,7 +16,7 @@ function SidebarNav({ isOpen }) {
     }
 
     return (
-        <nav className="p-8 ">
+        <nav className="p-8">
             <ul className="space-y-2">
                 {menuItems.map((item) => (
                     <NavLink
@@ -26,21 +24,28 @@ function SidebarNav({ isOpen }) {
                         to={item.href}
                         end={item.exact}
                         className={({ isActive }) =>
-                            `flex items-center py-2 px-4 rounded transition-colors duration-200 ${isActive ? "bg-primary text-secondary" : "hover:bg-primary hover:text-secondary"
-                            }`
+                            `flex items-center py-1 rounded-box transition-colors duration-200 ${isActive ? "bg-slate-700 text-lg" : "hover:bg-slate-500"}`
                         }
                     >
                         {({ isActive }) => (
-                            <li className="flex items-center justify-center py-2 px-4 rounded">
-                                <img className={`h-8 w-8 mr-2  ${isActive ? "animate-bounce" : ""}`} src={item.icon} alt={item.iconAlt} />
+                            <li className={`flex items-center justify-center font-semibold tracking-wide py-2 px-4 rounded relative text-sm ${isActive ? "text-base" : ""}`}>
+                                <img
+                                    className="h-6 w-6 mr-2"
+                                    src={item.icon}
+                                    alt={item.iconAlt}
+                                />
                                 {item.name}
+                                {item.showIndicator && (
+                                    <div className="indicator absolute -right-3 -top-1">
+                                        <span className="indicator-item badge badge-xs badge-success animate-pulse"></span>
+                                    </div>
+                                )}
                             </li>
                         )}
                     </NavLink>
                 ))}
             </ul>
         </nav>
-
     );
 }
 
