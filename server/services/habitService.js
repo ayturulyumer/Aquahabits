9b4ca216cb474d034de9b757d00c7ecdb1b6a7ca
ajummer <makeudreamempty@gmail.com>
2025-01-18 13:02:19 +0200

    fix : fix dates

--- server/services/habitService.js
+++ server/services/habitService.js
@@ -48,20 +48,24 @@ exports.checkInHabit = async (userId, habitId) => {
   }
 
   const today = new Date();
-  // Normalize the current date to only compare the date part (set time to midnight)
-  const normalizedToday = new Date(today.setHours(0, 0, 0, 0));
+  // Normalize the current date to midnight in UTC
+  const normalizedToday = new Date(
+    Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate())
+  );
 
   // Check if today is already in the history (compare only the date part)
   if (
     habit.history.some(
       (date) =>
-        new Date(date).setHours(0, 0, 0, 0) === normalizedToday.getTime()
+        new Date(date).toISOString().split("T")[0] ===
+        normalizedToday.toISOString().split("T")[0]
     )
   ) {
     // If already checked in, remove today's date (uncheck)
     habit.history = habit.history.filter(
       (date) =>
-        new Date(date).setHours(0, 0, 0, 0) !== normalizedToday.getTime()
+        new Date(date).toISOString().split("T")[0] !==
+        normalizedToday.toISOString().split("T")[0]
     );
     await habit.save();
     return { message: "Checkout", habit };
