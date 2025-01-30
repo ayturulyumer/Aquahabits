354d8df9f499a08350aec1f34a7d624f42e1d3ac
ajummer <makeudreamempty@gmail.com>
2025-01-30 10:23:30 +0200

    feat: add creatures route

--- server/controllers/creatureController.js
+++ server/controllers/creatureController.js
@@ -5,9 +5,11 @@ const creatureService = require("../services/creatureService");
 router.get("/", auth, async (req, res) => {
   try {
     const creatures = await creatureService.getAll();
-    res.json(creatures);
+    res.status(200).json(creatures);
   } catch (err) {
     const statusCode = err.message === "No creatures found" ? 404 : 500;
     res.status(statusCode).json({ message: err.message });
   }
 });
+
+module.exports = router;
--- server/routes.js
+++ server/routes.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const usersController = require("./controllers/usersController.js");
 const habitController = require("./controllers/habitController.js");
 const questController = require("./controllers/questController.js");
+const creatureController = require("./controllers/creatureController.js");
 
 router.use("/auth", usersController);
 router.use("/habits", habitController);
 router.use("/quests", questController);
+router.use("/creatures", creatureController);
 
 module.exports = router;
