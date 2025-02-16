edf6743fcb16b22bd2dbb77b956563c36c5e25dd
ajummer <makeudreamempty@gmail.com>
2025-02-16 11:42:04 +0200

    chore: return creatures sorted by ascending order

--- server/services/creatureService.js
+++ server/services/creatureService.js
@@ -1,7 +1,7 @@
 const Creature = require("../models/Creature");
 
 exports.getAll = async () => {
-  const creatures = await Creature.find().exec();
+  const creatures = await Creature.find().sort({ cost: 1 }).exec();
 
   if (!creatures) {
     throw new Error("No creatures found");
