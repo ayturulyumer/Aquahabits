1aa460f57c27933c0c5a9ce92af2fc10c2c5d4aa
ajummer <makeudreamempty@gmail.com>
2025-02-20 11:15:58 +0200

    deploy: add cors options

--- server/config/server.js
+++ server/config/server.js
@@ -5,17 +5,20 @@ const cors = require("cors");
 const isProduction = process.env.NODE_ENV === "production";
 
 const corsOptions = {
-  origin: isProduction ? "https://habitect-9qgr.vercel.app" : "http://localhost:5173",
+  origin: isProduction
+    ? "https://habitect-9qgr.vercel.app"
+    : "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Allow cookies and authentication headers
-}
+};
 
 const configureServer = (app) => {
+  app.use(cors(corsOptions));
+  app.options("*", cors(corsOptions)); // Handle preflight requests
   app.use(express.json()); // Parse JSON
   app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
   app.use(cookieParser()); // Parse cookies
-  app.use(cors(corsOptions));
 
   console.log("Server configuration loaded successfully !");
 };
