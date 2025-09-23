10199f2236d5a8a82011954811b31ccd35d6d06b
ajummer <makeudreamempty@gmail.com>
2025-09-23 12:21:26 +0300

    feat: add github to footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -50,6 +50,9 @@ export default function Footer() {
                     </a>
                 </div>
             </nav>
+            <p>
+                Built with ❤️ by <a className="mx-auto font-bold" href="https://github.com/ayturulyumer" target="_blank" rel="noopener noreferrer">Ayti</a>
+            </p>
             <p className="text-center ">
                 All icons by <a href="https://www.flaticon.com/search?author_id=1&style_id=&type=standard&word=sea+creatures" target="_blank" rel="noopener noreferrer">Freepik - Flaticon</a>
             </p>
